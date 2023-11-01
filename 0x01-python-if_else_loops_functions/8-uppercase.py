bdc9a42b40032f04c8a9f00a1ba5c6efa327706f
nehadadel <eng.nehadadel@gmail.com>
2023-10-31 17:16:29 -0700

    task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
         if ord(char) >= 97 and ord(char) <= 122:
             print("{}".format(chr(ord(char) - 32)), end="")
         else:
-            print("{}".format(char))
+            print("{}".format(char), end="")
