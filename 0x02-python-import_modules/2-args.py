a474cab27de570a045762cae5a3284d4df566a25
nehadadel <eng.nehadadel@gmail.com>
2023-11-02 16:28:29 -0700

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-a = 3
+from sys import argv
+if __name__ == "__main__":
+    print("{} arguments".format(len(argv) - 1), end="")
+    if len(argv) - 1 == 0:
+        print(".")
+    else:
+        print(":")
+        i = 1
+        while i <= len(argv) - 1:
+            print("{}: {}".format(i, argv[i]))
+            i += 1
