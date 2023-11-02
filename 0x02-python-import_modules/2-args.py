788952f5aa7a0944dd2f45b738c6f1501701692c
nehadadel <eng.nehadadel@gmail.com>
2023-11-02 16:35:00 -0700

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    print("{} arguments".format(len(argv) - 1), end="")
+    print("{} argument".format(len(argv) - 1), end="")
     if len(argv) - 1 == 0:
-        print(".")
-    else:
+        print("s.")
+    elif len(argv) - 1 == 1:
         print(":")
+        print("1: {:s}".format(argv[1]))
+    else:
+        print("s:")
         i = 1
         while i <= len(argv) - 1:
             print("{:d}: {:s}".format(i, argv[i]))
