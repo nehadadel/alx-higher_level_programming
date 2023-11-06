c2aaee1313aca71957316a4616b29cb9824f7687
nehadadel <eng.nehadadel@gmail.com>
2023-11-06 12:10:43 -0800

    task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+    else:
+        for row in matrix:
+            for column in row:
+                print("{:d}".format(column), end="")
+                if column == row[-1]:
+                    print("")
+                else:
+                    print(" ", end="")
