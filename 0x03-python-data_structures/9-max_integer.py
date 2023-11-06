9f11453a50c6b4c03c9acc038c7ecd6c9abd62fd
nehadadel <eng.nehadadel@gmail.com>
2023-11-06 14:16:45 -0800

    task6

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    else:
+        max = my_list[0]
+        for item in my_list:
+            if item > max:
+                max = item
+        return max
+            
