a28c4fb44e7deb121b1c291fe4062164561f580a
nehadadel <eng.nehadadel@gmail.com>
2023-11-08 15:09:47 -0800

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    i = 0
-    new_list = []
-    while(i < len(my_list)):
-        if my_list[i] == search:
-            new_list.append(replace)
-        else:
-            new_list.append(my_list[i])
-        i += 1
+    new_list = [num if num != search else replace for num in my_list]
     return new_list
