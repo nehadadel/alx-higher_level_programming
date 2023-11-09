6ff8b8b9418ce0f3b740e06724869de9efcb348d
nehadadel <eng.nehadadel@gmail.com>
2023-11-08 18:04:39 -0800

    intrview

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,15 +4,15 @@ def roman_to_int(roman_string):
         return 0
     else:
         count = 0
-        roman_number = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
-        roman_number2 = {'IV' : 4, 'IX' : 9, 'XL' : 40, 'XC' : 90, 'CD' : 400, 'CM' : 900}
-        for key in roman_number2:
+        r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        r_num2 = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        for key in r_num2:
             if key in roman_string:
-                count += roman_number2.get(key)
+                count += r_num2.get(key)
                 roman_string = roman_string.replace(key, "")
         for ch in roman_string:
-            for key in roman_number:
+            for key in r_n:
                 if ch == key:
-                    count += roman_number.get(key)
+                    count += r_n.get(key)
                     break
         return count
