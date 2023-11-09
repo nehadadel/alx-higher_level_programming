9ef23ed865942b3f775c73a34f02a2bb03c4f831
nehadadel <eng.nehadadel@gmail.com>
2023-11-08 16:46:03 -0800

    task1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    else:
-        best_name = max(a_dictionary, key=lambda key: a_dictionary[key])
-        return best_name
+    if a_dictionary is not None:
+        students_number = len(a_dictionary.values())
+        if students_number > 0:
+            score = max(a_dictionary.values())
+            for key in a_dictionary.keys():
+                if a_dictionary[key] == score:
+                    return key
+    return None
