133e2ce2a9647aa24cf4f362770ebb87d680dca7
nehadadel <eng.nehadadel@gmail.com>
2023-11-21 16:00:24 -0800

    try

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,11 +9,10 @@ defining the square
 
 class Square:
     """a 2d square"""
-    try:
-        def __init__(self, size=0):
-            self.__size = size
-    except TypeError:
-        print("size must be an integer")
-        
-
+    def __init__(self, size=0):
+        if size is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
     pass
