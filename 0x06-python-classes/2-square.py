262c4c453a197cc56dfa63eb5c4c4adcf6fbb88a
nehadadel <eng.nehadadel@gmail.com>
2023-11-21 16:06:37 -0800

    try

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,8 +10,9 @@ defining the square
 class Square:
     """a 2d square"""
     def __init__(self, size=0):
-        if not type(x) is int:
+        if not type(size) is int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    pass
