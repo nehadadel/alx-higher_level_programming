0857787dbe7959389970826feb8b9615627a88c7
nehadadel <eng.nehadadel@gmail.com>
2023-11-27 15:09:37 -0800

    0task

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,7 +38,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-
     def area(self):
         """Rectangle area getter"""
         return self.__height * self.__width
