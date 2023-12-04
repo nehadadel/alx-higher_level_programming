2772715669b557bdb22c4736b65ea0885cb05a71
nehadadel <eng.nehadadel@gmail.com>
2023-12-04 15:10:28 -0800

    3

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeometry """
+
+    def area(self):
+        """ area """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ integer_validaattor """
+        if type(value) != int:
+            raise TypeError(name + ' must be an integer')
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
+        
