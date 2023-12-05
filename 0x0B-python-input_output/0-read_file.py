53fec7a039bdabc9476e10baa1f6c2852acaa636
nehadadel <eng.nehadadel@gmail.com>
2023-12-05 14:53:34 -0800

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ read file """
-    with open(filename, 'r', encoding="UTF8") as f:
+    with open(filename, 'r', encoding="UTF-8") as f:
         read_data = f.read()
         print(read_data, end="")
