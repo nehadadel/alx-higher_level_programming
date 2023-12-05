83f284edb2f5dcff7398bda7f4cbf025a1f9af5f
nehadadel <eng.nehadadel@gmail.com>
2023-12-05 15:26:45 -0800

    o

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3                                                                                                                   
+"""json"""
+
+
+def to_json_string(my_obj):
+    """json"""
+    return json.dumps(my_obj)
