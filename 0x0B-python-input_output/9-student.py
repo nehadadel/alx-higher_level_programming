39c07be1e4bcd401a0d3af9b4df52d657befc927
nehadadel <eng.nehadadel@gmail.com>
2023-12-06 14:11:49 -0800

    t3

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""class student"""
+
+
+class Student:
+    """Stdudent"""
+
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ to _json """
+        return vars(self.__dict__)
