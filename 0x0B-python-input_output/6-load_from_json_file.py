276004751985472b9595059cfc2e28c58e96f6f7
nehadadel <eng.nehadadel@gmail.com>
2023-12-05 16:17:44 -0800

    o

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""json"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ load from json file"""
+    with open(filename, "r") as read_file:
+        data = json.load(read_file)
