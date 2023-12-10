5b384843f734ec0326bb6e623032d745d59478e5
nehadadel <eng.nehadadel@gmail.com>
2023-12-10 15:36:23 -0800

    try

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+
+"""
+class Rectangle
+"""
+
+from models.base import Base
+class Rectangle(Base):
+    """class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(id)
+        self.integer_validator("width", width, False)
+        self.integer_validator("height", height, False)
+        self.integer_validator("x", x)
+        self.integer_validator("y", y)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, width):
+        self.integer_validator("width", width, False)
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        self.integer_validator("height", height, False)
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        self.integer_validator("x", x)
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        self.integer_validator("y", y)
+        self.__y = y
+
+    def integer_validator(self, name, value, flag = True):
+        """integer_validator function"""
+        if type(value) != int:
+            raise TypeError(name + ' must be an integer')
+        if value <= 0 and flag == False:
+            raise ValueError(name + ' must be > 0')
+        elif value < 0 and flag == True:
+            raise ValueError(name + ' must be >= 0')
