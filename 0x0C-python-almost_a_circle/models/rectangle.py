2eff4d5a32c506567e99d9376b055c29cb480ad7
nehadadel <eng.nehadadel@gmail.com>
2023-12-11 15:47:03 -0800

    t

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,7 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        "that returns the dictionary representation of a Rectangle:"
+        return vars(self)
