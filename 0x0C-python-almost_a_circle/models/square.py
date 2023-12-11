2b18de87e5fbba94852df6ee9b913287f93e9264
nehadadel <eng.nehadadel@gmail.com>
2023-12-11 15:10:16 -0800

    t

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+class Square
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
