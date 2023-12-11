f57cc81e1c8dc9c082e415c10d21b3049c7b003c
nehadadel <eng.nehadadel@gmail.com>
2023-12-11 15:36:53 -0800

    t

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,13 @@ class Square(Rectangle):
         self.integer_validator("width", value, False)
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        "that assigns attributes:"
+        attributes = ["id", "size", "x", "y"]
+        if args:
+            for i, value in enumerate(args):
+                setattr(self, attributes[i], value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
