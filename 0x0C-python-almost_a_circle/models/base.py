3cb03c2af4ab97347c77341efa4011cd41bfffd1
nehadadel <eng.nehadadel@gmail.com>
2023-12-11 16:04:17 -0800

    t

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 
+import json
 """class base:
 This class will be the “base” of all other classes in this project.
 The goal of it is to manage id attribute in all your future classes
@@ -19,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        " that returns the JSON string representation of list_dictionaries"
+        if list_dictionaries is []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
