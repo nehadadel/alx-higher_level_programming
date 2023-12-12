cbce357485793ba5f7bed1661f288155dbef6d68
nehadadel <eng.nehadadel@gmail.com>
2023-12-11 16:12:34 -0800

    t

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         " that returns the JSON string representation of list_dictionaries"
-        if list_dictionaries is []:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
