94053be46720ded81f64b0c14cba6980ed917548
nehadadel <eng.nehadadel@gmail.com>
2024-02-26 02:31:37 +0300

    copy 0x06

---
+++ 0x06-python-classes_copy/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square Class
+
+defining the square
+
+"""
+
+
+class Square:
+    """a 2d square"""
+    pass
---
+++ 0x06-python-classes_copy/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square Class
+
+defining the square
+
+"""
+
+
+class Square:
+    """a 2d square"""
+    def __init__(self, size):
+        self.__size = size
+
+    pass
