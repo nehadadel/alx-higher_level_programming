f91dfdec5125bd4d08077c94053701ea8a2e79c4
nehadadel <eng.nehadadel@gmail.com>
2024-03-02 15:54:49 -0800

    t1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
-"""define a Rectangle"""
+"""defines a Rectangle"""
 
 
 class Rectangle:
-    """Rectangle class"""
-
-    def __init__(self, width=0, height=0):
-        """init for Rectangle"""
-        self.width = width
-        self.height = height
+    '''A simple class representing a rectangle. '''
+    @staticmethod
+    def value_validation(value, name):
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value < 0:
+            raise ValueError(name +" must be >= 0")
 
     @property
     def width(self):
-        """Rectangle width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Rectangle width setter"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        self.value_validation(value, "width")
         self.__width = value
 
     @property
     def height(self):
-        """Rectangle height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Rectangle height setter"""
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        self.value_validation(value, "height")
         self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.value_validation(width, "width")
+        self.__width = width
+        self.value_validation(height, "height")
+        self.__height = height
