02a9782726207245eb55c21eef1e72f53e20e1b2
nehadadel <eng.nehadadel@gmail.com>
2024-03-02 16:07:01 -0800

    t1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,49 +1,45 @@
 #!/usr/bin/python3
-"""define a Rectangle"""
+"""defines a Rectangle"""
 
 
 class Rectangle:
-    """Rectangle class"""
-
-    def __init__(self, width=0, height=0):
-        """init for Rectangle"""
-        self.width = width
-        self.height = height
+    '''A simple class representing a rectangle. '''
+    @staticmethod
+    def value_validation(value, name):
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value < 0:
+            raise ValueError(name + " must be >= 0")
 
     @property
     def width(self):
-        """Rectangle width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Rectangle width setter"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        self.value_validation(value, "width")
         self.__width = value
 
     @property
     def height(self):
-        """Rectangle height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Rectangle height setter"""
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        self.value_validation(value, "height")
         self.__height = value
 
+    def __init__(self, width=0, height=0):
+        self.value_validation(width, "width")
+        self.__width = width
+        self.value_validation(height, "height")
+        self.__height = height
+
     def area(self):
-        """Rectangle area getter"""
-        return self.__height * self.__width
+        return self.__heigth * self.__width
 
     def perimeter(self):
-        """perimeter getter"""
         if self.__height == 0 or self.__width == 0:
             return 0
-        return (self.__height + self.__width) * 2
+        else:
+            return 2 * (self.__heigth + self.__width)
