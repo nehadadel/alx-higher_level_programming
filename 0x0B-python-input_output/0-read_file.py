8b8c0068654d0a762626dd1fe459ae95cd8fadd8
nehadadel <eng.nehadadel@gmail.com>
2024-03-13 12:05:03 -0700

    t

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""task0"""
+
+
 def read_file(filename=""):
     """that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, 'r', encoding="utf-8") as f:
         data = f.read()
-        print(data)
+        print(data, end="")
