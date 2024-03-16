1ba6d7c75f1108f856f7d7101ad39efd1df846fb
nehadadel <eng.nehadadel@gmail.com>
2024-03-16 13:25:25 -0700

    add 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+import json
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return vars(self)
