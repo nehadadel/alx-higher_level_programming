9cf829789ab87bf0a8481cb8ddf7c856780470f4
nehadadel <eng.nehadadel@gmail.com>
2024-03-23 17:01:01 -0700

    load

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""student class"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary
+
+    def reload_from_json(self, json):
+        """that replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
