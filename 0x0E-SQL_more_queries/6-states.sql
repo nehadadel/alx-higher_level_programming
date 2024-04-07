d07fc026c8ef27e25114e4887778655503ece0e3
nehadadel <eng.nehadadel@gmail.com>
2024-04-06 22:13:48 -0700

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,4 +3,4 @@
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
