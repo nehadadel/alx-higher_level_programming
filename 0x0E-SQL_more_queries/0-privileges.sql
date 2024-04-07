1bff8efee126741f23d705cdfe097c3d0a3c5434
nehadadel <eng.nehadadel@gmail.com>
2024-04-06 20:04:45 -0700

    sql1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1
 -- and user_0d_2 on your server (in localhost).
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
