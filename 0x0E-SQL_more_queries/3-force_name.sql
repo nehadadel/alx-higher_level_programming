ae4d184ceb8dbad9320d51fe9b3990c21fc16a34
nehadadel <eng.nehadadel@gmail.com>
2024-04-06 20:54:13 -0700

    sql1

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- creates the table force_name on your MySQL server.
+-- force_name description: id INT name VARCHAR(256) can’t be null
+create table IF NOT EXISTS first_table (id INT, name VARCHAR(256) NOT NULL);
