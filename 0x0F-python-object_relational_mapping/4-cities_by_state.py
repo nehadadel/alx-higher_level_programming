4d7040c868e9fa3941bdcd02c851a6e3950e1f2a
nehadadel <eng.nehadadel@gmail.com>
2024-04-17 13:24:19 -0700

    h

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""script that lists all cities from the database hbtn_0e_4_usa"""
 
+"""
+    A script that lists all cities from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
+"""
+
+
+import sys
 import MySQLdb
-from sys import argv
 
-# The code should not be executed when imported
+
 if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
-    cur = db.cursor()
+    cursor = db.cursor()
+
     sql = """SELECT c.id, c.name, s.name
           FROM states s, cities c
           WHERE c.state_id = s.id
           ORDER BY c.id ASC"""
+
     cursor.execute(sql)
 
     data = cursor.fetchall()
