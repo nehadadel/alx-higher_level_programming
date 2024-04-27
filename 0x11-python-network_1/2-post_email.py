3df3e2b276980414387f7b617bf46abcb05b4c47
nehadadel <eng.nehadadel@gmail.com>
2024-04-26 18:23:49 -0700

    t2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL and an email, 
+"""Write a Python script that takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
