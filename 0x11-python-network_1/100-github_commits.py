fb9fd1ef87a1daac11fce6fea32c192999b50df0
nehadadel <eng.nehadadel@gmail.com>
2024-04-28 15:31:48 -0700

    adv

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """
+Lists the 10 most recent commits on a given GitHub repository.
+
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 
 
@@ -11,13 +14,16 @@ if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
     Path_parameters = {'owner': owner_name, 'repo': repository_name}
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1])
-
-
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[2], sys.argv[1])
     r = requests.get(url, params=Path_parameters)
     commits = r.json()  # Get the list of commits
 
-    for commit in commits:
-        commit_sha = commit['sha']  # Access the 'sha' attribute of each commit
-        author = commit['committer']
-        print("{}: {}".format(commit_sha, author.get("name")))
+    try:
+        for i in range(10):
+            # Access the 'sha' attribute of each commit
+            commit_sha = commits[i].get("sha")
+            author = commits[i].get("commit").get("author")
+            print("{}: {}".format(commit_sha, author.get("name")))
+    except IndexError:
+        pass
