64601ef93daee27365b430fde6a2de8131ae4aec
nehadadel <eng.nehadadel@gmail.com>
2024-04-28 11:48:10 -0700

    t5

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter
+and finally displays the body of the response
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    value = {"email": sys.argv[2]}
+    r = requests.get(url, data=value)
+
+    print(r.text)
+
+    
