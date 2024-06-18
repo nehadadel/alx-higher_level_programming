cc36c568ab92b1406ca947958c71673b7a59e84b
nehadadel <eng.nehadadel@gmail.com>
2024-06-18 14:51:52 -0700

    1task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length > 2) {
+} else {
   console.log(process.argv[2]);
 }
