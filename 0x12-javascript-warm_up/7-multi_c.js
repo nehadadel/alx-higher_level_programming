86494a276f8a868c8024c2fb5e400aa486c1b575
nehadadel <eng.nehadadel@gmail.com>
2024-06-18 15:41:56 -0700

    1task

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ if (isNaN(Number(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
-  console.log('C is fun');
-}
+    console.log('C is fun');
+  }
 }
