721f30589691de8d0f173535e5418ea3bf9a2663
nehadadel <eng.nehadadel@gmail.com>
2024-06-23 14:45:15 -0700

    sqlalchamy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""
+file that contains the class definition of
+a State and an instance Base = declarative_base():
+
+State class:
+inherits from Base Tips
+
+links to the MySQL table states
+
+class attribute id that represents a column of an auto-generated,
+unique integer, can’t be null and is a primary key
+
+class attribute name that represents a column of a string
+with maximum 128 characters and can’t be null
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
