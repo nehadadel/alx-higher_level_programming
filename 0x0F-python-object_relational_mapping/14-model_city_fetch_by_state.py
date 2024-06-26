c635d921827d4a8bec105ebdf9bdb7af643d30a6
nehadadel <eng.nehadadel@gmail.com>
2024-06-26 06:23:55 -0700

    city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,9 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    results = session.query(City, State).order_by(City.id).all()
+    results = session.query(City, State).\
+        filter(City.state_id == State.id).all()
     for city, state in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.commit()
     session.close()
